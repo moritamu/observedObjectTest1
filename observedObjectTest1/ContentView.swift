9da9b778c79bf035ab72cd1eaac7b22628c4ada6
morisum <moris@r.sannet.ne.jp>
2024-04-30 06:39:35 +0900

    Initial Commit

---
+++ observedObjectTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  observedObjectTest1
+//
+//  Created by MsMacM on 2024/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ observedObjectTest1/observedObjectTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  observedObjectTest1App.swift
+//  observedObjectTest1
+//
+//  Created by MsMacM on 2024/04/30.
+//
+
+import SwiftUI
+
+@main
+struct observedObjectTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
